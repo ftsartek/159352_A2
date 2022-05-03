534211ff049bd8f76baf67ec2bb43f8888bbb0c2
Jordan Russell <jordanbrussell@gmail.com>
2022-05-03 15:14:15 +1200

    Add form import

--- app/__init__.py
+++ app/__init__.py
@@ -27,4 +27,4 @@ app.config['SESSION_TYPE'] = 'filesystem'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-from app import routes, database, accounts
+from app import routes, database, accounts, forms
