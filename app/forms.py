cd6227f0973bf717800540b58096b90cfc92168e
Jordan Russell <jordanbrussell@gmail.com>
2022-05-03 15:15:46 +1200

    Add csrf security to forms

--- app/forms.py
+++ app/forms.py
@@ -1,12 +1,16 @@
-from wtforms import Form, TextAreaField, SelectField, BooleanField, DateField, StringField, PasswordField, IntegerField, validators
+from app import app
+from flask_wtf import FlaskForm, csrf
+from wtforms import TextAreaField, SelectField, BooleanField, DateField, StringField, PasswordField, IntegerField, validators
+
+csrf = csrf.CSRFProtect(app)
 
 
-class LoginForm(Form):
+class LoginForm(FlaskForm):
     email = StringField('Email', [validators.Length(min=4, max=30)])
     password = PasswordField('Password', [validators.DataRequired(), validators.Length(min=8, max=25)])
 
 
-class RegistrationForm(Form):
+class RegistrationForm(FlaskForm):
     email = StringField('Email', [validators.Length(min=4, max=30)])
     password = PasswordField('Password', [validators.DataRequired(), validators.Length(min=8, max=25)])
     confirm = PasswordField('Confirm Password', [validators.DataRequired(), validators.Length(min=8, max=25)])
