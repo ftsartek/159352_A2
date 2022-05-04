a09fe7945b735996c284a245859e5ed533d7c600
Jordan Russell <jordanbrussell@gmail.com>
2022-05-04 13:09:29 +1200

    Add custom handling for unauthorised users/requests

--- app/accounts.py
+++ app/accounts.py
@@ -1,3 +1,4 @@
+from flask import flash, redirect
 from flask_login import LoginManager
 from app import app, database
 
@@ -8,4 +9,10 @@ login_manager.init_app(app)
 
 @login_manager.user_loader
 def load_user(user_id):
-    return database.User.query.filter_by(id=user_id).first()
+    return database.User.query.filter_by(id=int(user_id)).first()
+
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    flash('This resource or page requires you to be logged in.', 'danger')
+    return redirect('/login')
